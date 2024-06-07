60bd1fb3b5b8a0d633615d3c8ea80a567bfd3df1
souravsarker2015 <souravsarker2015@gmail.com>
2024-06-08 04:38:35 +0600

    models declared

--- apps/expenses/models.py
+++ apps/expenses/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from apps.core.models import BaseModel
+
+EXPENSE_TYPE_CHOICES = [
+    ('bank', 'Bank'),
+    ('individual', 'Individual'),
+    ('corporation', 'Corporation'),
+    ('shop', 'Shop'),
+    ('family', 'Family'),
+]
+
+
+class Expense(BaseModel):
+    expense_date = models.DateField()
+    expense_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    expense_type = models.CharField(max_length=30, choices=EXPENSE_TYPE_CHOICES)
+    expense_details = models.TextField()
--- apps/sells/models.py
+++ apps/sells/models.py
@@ -1,3 +1,12 @@
+from apps.core.models import BaseModel
+
 from django.db import models
 
-# Create your models here.
+
+class Sell(BaseModel):
+    sell_date = models.DateField()
+    sell_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sell_image = models.ImageField(upload_to='sell_images', null=True, blank=True)
+    sell_place = models.CharField(max_length=254, null=True, blank=True)
+    buyer = models.CharField(max_length=254, null=True, blank=True)
+    sell_description = models.TextField(null=True, blank=True)
